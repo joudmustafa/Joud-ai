8be5ab0b2ceec92cbcb46180c325cb914cc71188
joudmustafa <joodabdullatief@gmail.com>
2026-01-17 19:06:53 +0200

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,34 +1,31 @@
-// Show the pop-up automatically when the page loads
-window.onload = function() {
-    document.getElementById('popup-nl').style.display = 'flex';
-  };
-  
-  // Close the pop-up when the user clicks the close button
-  document.querySelector('.close-nl').addEventListener('click', function() {
-    document.getElementById('popup-nl').style.display = 'none';
-  });
-  
-  // // Close the pop-up when clicking outside the pop-up content
-  // window.addEventListener('click', function(event) {
-  //   const popupContent = document.querySelector('.popup-content'); // Select the popup content
-  //   if (event.target === document.getElementById('popup')) {
-  //       document.getElementById('popup').style.display = 'none';
-  //   }
-  // });
-  
-  // Handle form submission
-  document.getElementById('emailForm-nl').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    const email = document.getElementById('email-nl').value;
-    if (email) {
-      alert(`Your email ID ${email} has been registered successfully for the newsletter.`);
-      document.getElementById('popup').style.display = 'none';
-    }
-  });
-  
-  // Handle "No thanks" link
-  document.querySelector('.no-thanks-nl').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('popup-nl').style.display = 'none';
-  });
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+function openModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function submitForm(formId) {
+  var form = document.getElementById(formId);
+  if (form.checkValidity()) {
+    form.submit();
+  } else {
+    form.reportValidity();
+  }
+}
